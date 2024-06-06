bc57c17d53d2c09b183081a562d92ec74d3150a3
Akhil <akhilkum249@gmail.com>
2024-06-06 09:58:29 +0530

    card created

--- block-BRaaai/components/card/src/components/card.jsx
+++ block-BRaaai/components/card/src/components/card.jsx
@@ -1,6 +1,5 @@
 let cardInfo = {
     title: 'City Lights in New York',
-    date: new Date('2017-01-26'),
     comments: 67,
     subTitle: 'The city that never sleeps.',
     description:
@@ -9,3 +8,26 @@ let cardInfo = {
     imageURL:
       'https://images.unsplash.com/photo-1610874933184-7728fd666162?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60',
   };
+function Card(){
+    return(
+        <>
+        <header>
+        <div className="mydiv">
+            <div className="crcl">
+                <p>{cardInfo.date}</p>
+            </div>
+       <img src={cardInfo.imageURL} alt="sandimage" />
+       <div className="container">
+       <h2>{cardInfo.title}</h2>
+       <h3>{cardInfo.subTitle}</h3>
+       <p>{cardInfo.description}</p>
+      
+        </div>
+       </div>
+        </header>
+       
+      
+        </>
+    )
+}
+export default Card;
